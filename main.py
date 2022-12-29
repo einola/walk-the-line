87a4c6fbf426270126a86c77ebfe3e5f79e099e3
Einar Örn Ólason <einar.olason@nersc.no>
2022-12-29 09:53:36 +0000

    Bug fixes and comments

--- main.py
+++ main.py
@@ -1,25 +1,57 @@
-sensorDifference = 0
-leftSensor = 0
+sensorSensitivity = 0
+speed = 0
+driving = False
 rightSensor = 0
-Kitronik_Move_Motor.motor_balance(Kitronik_Move_Motor.SpinDirections.RIGHT, 3)
-sensorSensitivity = 10
-speed = 30
+leftSensor = 0
+sensorDifference = 0
+
+def on_button_pressed_a():
+    global sensorSensitivity, speed, driving, rightSensor, leftSensor, sensorDifference
+    # Just for this Kitronik Move
+    Kitronik_Move_Motor.motor_balance(Kitronik_Move_Motor.SpinDirections.RIGHT, 3)
+    Kitronik_Move_Motor.set_ultrasonic_units(Kitronik_Move_Motor.Units.CENTIMETERS)
+    # Sensitivity of the optical sensor
+    sensorSensitivity = 10
+    # Speed of the robot
+    speed = 30
+    # count down!
+    basic.show_number(3)
+    basic.show_number(2)
+    basic.show_number(1)
+    Kitronik_Move_Motor.beep_horn()
+    # Drive!
+    driving = True
+    while driving:
+        # Read sensors
+        rightSensor = Kitronik_Move_Motor.read_sensor(Kitronik_Move_Motor.LfSensor.LEFT)
+        leftSensor = Kitronik_Move_Motor.read_sensor(Kitronik_Move_Motor.LfSensor.RIGHT)
+        sensorDifference = leftSensor - rightSensor
+        if sensorDifference > sensorSensitivity:
+            # We need to turn right
+            Kitronik_Move_Motor.motor_off(Kitronik_Move_Motor.Motors.MOTOR_RIGHT)
+            Kitronik_Move_Motor.motor_on(Kitronik_Move_Motor.Motors.MOTOR_LEFT,
+                Kitronik_Move_Motor.MotorDirection.FORWARD,
+                speed)
+        elif sensorDifference < 0 - sensorSensitivity:
+            # We need to turn left
+            Kitronik_Move_Motor.motor_off(Kitronik_Move_Motor.Motors.MOTOR_LEFT)
+            Kitronik_Move_Motor.motor_on(Kitronik_Move_Motor.Motors.MOTOR_RIGHT,
+                Kitronik_Move_Motor.MotorDirection.FORWARD,
+                speed)
+        else:
+            # Hold the course
+            Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.FORWARD, speed)
+        # Crash protection!
+        if Kitronik_Move_Motor.measure() < 15:
+            Kitronik_Move_Motor.stop()
+            Kitronik_Move_Motor.beep_horn()
+            driving = False
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    Kitronik_Move_Motor.stop()
+input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_forever():
-    global rightSensor, leftSensor, sensorDifference
-    rightSensor = Kitronik_Move_Motor.read_sensor(Kitronik_Move_Motor.LfSensor.LEFT)
-    leftSensor = Kitronik_Move_Motor.read_sensor(Kitronik_Move_Motor.LfSensor.RIGHT)
-    sensorDifference = leftSensor - rightSensor
-    if sensorDifference > sensorSensitivity:
-        Kitronik_Move_Motor.motor_off(Kitronik_Move_Motor.Motors.MOTOR_LEFT)
-        Kitronik_Move_Motor.motor_on(Kitronik_Move_Motor.Motors.MOTOR_LEFT,
-            Kitronik_Move_Motor.MotorDirection.FORWARD,
-            speed)
-    elif sensorDifference < 0 - sensorSensitivity:
-        Kitronik_Move_Motor.motor_off(Kitronik_Move_Motor.Motors.MOTOR_RIGHT)
-        Kitronik_Move_Motor.motor_on(Kitronik_Move_Motor.Motors.MOTOR_RIGHT,
-            Kitronik_Move_Motor.MotorDirection.FORWARD,
-            speed)
-    else:
-        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.FORWARD, speed)
+    pass
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -5,28 +5,38 @@ let rightSensor = 0
 let leftSensor = 0
 let sensorDifference = 0
 input.onButtonPressed(Button.A, function () {
+    // Just for this Kitronik Move
     Kitronik_Move_Motor.motorBalance(Kitronik_Move_Motor.SpinDirections.Right, 3)
     Kitronik_Move_Motor.setUltrasonicUnits(Kitronik_Move_Motor.Units.Centimeters)
+    // Sensitivity of the optical sensor
     sensorSensitivity = 10
+    // Speed of the robot
     speed = 30
+    // count down!
     basic.showNumber(3)
     basic.showNumber(2)
     basic.showNumber(1)
     Kitronik_Move_Motor.beepHorn()
+    // Drive!
     driving = true
     while (driving) {
+        // Read sensors
         rightSensor = Kitronik_Move_Motor.readSensor(Kitronik_Move_Motor.LfSensor.Left)
         leftSensor = Kitronik_Move_Motor.readSensor(Kitronik_Move_Motor.LfSensor.Right)
         sensorDifference = leftSensor - rightSensor
         if (sensorDifference > sensorSensitivity) {
-            Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorLeft)
+            // We need to turn right
+            Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorRight)
             Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, speed)
         } else if (sensorDifference < 0 - sensorSensitivity) {
-            Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorRight)
+            // We need to turn left
+            Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorLeft)
             Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, speed)
         } else {
+            // Hold the course
             Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, speed)
         }
+        // Crash protection!
         if (Kitronik_Move_Motor.measure() < 15) {
             Kitronik_Move_Motor.stop()
             Kitronik_Move_Motor.beepHorn()
